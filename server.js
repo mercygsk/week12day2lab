e256874afbea632f28a351e27e23212107b5e2d7
mercygsk <mercygs.kodali@gmail.com>
2023-12-06 15:50:33 -0500

    third  express lab

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Create our express app
+const magic8ball = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
+// Create our express app
+app.get('/magic/:indexOfmagic8ballArray', (req, res) => {
+    if (req.params.indexOfmagic8ballArray == "Will I Be A Millionaire") {
+    res.send('Question:' + req.params.indexOfmagic8ballArray + " : <h1>" + magic8ball[Math.floor(Math.random()*(magic8ball.length - 0 + 1)+0)] + "</h1>");
+    }
+    else {
+        res.send('cannot find anything at this index: ' + req.params.indexOfmagic8ballArray);
+    }
+});
 
 // Define a "root" route directly on app
 // Tomorrow, we'll use best practice routing
